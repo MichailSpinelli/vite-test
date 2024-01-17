f7ebb7b841dfc66cb51b951494084cb9f0c326f1
Michail Spinelli <michailspinelli96@gmail.com>
2024-01-17 13:46:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  data() {
+    return { count: 0 }
+  },
+  components: {
+    AppHeader
+  }
+};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <h1>
+    ciao
+  </h1>
+  <p>
+    {{ count }}
+  </p>
+
+  <button @click="count++">
+    incrementa count
+  </button>
+</template>
+
+<style scoped>
+</style>
+
